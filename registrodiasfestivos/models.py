0806a5d8024c60b0730b2480fd7ab5a23c8dc25b
sergio rill <scrill182@gmail.com>
2019-04-22 17:37:02 -0500

    Mi aplicación Django Girls, primer commit

---
+++ registrodiasfestivos/models.py
@@ -0,0 +1,29 @@
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class registro (models.Model):
+
+
+    no_cobro=models.IntegerField( null=False)
+    nombre= models.CharField(max_length=200)
+    horario = models.CharField(max_length=200)
+    fecha_creacion= models.DateTimeField( default=timezone.now())
+    diasFestivos = models.DateField(null=False)
+
+
+
+    def creacion(self):
+        self.fecha_creacion = timezone.now()
+        self.save()
+
+
+    def __str__(self):
+        return self.nombre
+
+
+
+
+
