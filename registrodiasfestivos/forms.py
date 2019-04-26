e0ad119cfd04f3717dc693a6ed16a8debfbbdaa1
sergio rill <scrill182@gmail.com>
2019-04-26 18:24:18 -0500

    Forms 2

--- registrodiasfestivos/forms.py
+++ registrodiasfestivos/forms.py
@@ -1,5 +1,7 @@
 
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from .models import Registro
 
 
@@ -7,4 +9,10 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Registro
-        fields=('nombre','no_cobro','diasFestivos',)
+        fields = ('nombre','no_cobro','diasFestivos','horario')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Save person'))
